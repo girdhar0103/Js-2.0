2b86174ff0c4945315a2b2c9763db7381b412a5d
Girdhar Kumar <kumargirdhar804@gmail.com>
2025-05-03 18:14:03 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"Use strict"; // treat all js code as newer version
+
+console.log(3 + 3);
+
+let name = "Girdhar"      // string
+let age = 18             // number
+let isLoggedIn = false   // boolean
+
+
+/* Premitive data type */
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// nill => standalone value 
+// undefined =>
+// symbol => unique
+
+
+/* nonpremitive data type */
+
+// object
+// Array
+
+console.log(typeof "Girdhar"); // string
+console.log(typeof age); // number
+
