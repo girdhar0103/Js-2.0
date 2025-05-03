30d6c75fe7df12e5fc51590e085f3c2aa891aa34
Girdhar Kumar <kumargirdhar804@gmail.com>
2025-05-03 17:25:00 +0000

    let, const, var done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1236454
+let accountEmail = "kumargirdhar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
