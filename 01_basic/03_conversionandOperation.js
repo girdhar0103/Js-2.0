8868b77f7999db3570b036421283c1483cbd394e
Girdhar Kumar <kumargirdhar804@gmail.com>
2025-05-04 10:44:03 +0000

    conversion and operation

---
+++ 01_basic/03_conversionandOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"     // string
+// let score = "33"     // string
+// let score = null     // 0
+// let score = 33       // number
+// let score = undeined // NaN
+// let score = true     // 1
+// let score = alse     // 0
+// let score = "Girdhar"// NaN
+
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1         // true
+// let isLoggedIn = ""        // False
+// let isLoggedIn = "Girdhar" // true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
